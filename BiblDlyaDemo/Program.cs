7e738219ff352330b8e929160bd99dfae9e0194b
stari <stari@DESKTOP-7IQEQL4>
2022-06-24 08:39:24 +0300

    Добавьте файлы проекта.

---
+++ BiblDlyaDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SF2022User_NN_Lib;
+
+namespace BiblDlyaDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ts1 = TimeSpan.Parse("10:00");
+            TimeSpan ts2 = TimeSpan.Parse("11:00");
+            TimeSpan ts3 = TimeSpan.Parse("15:00");
+            TimeSpan ts4 = TimeSpan.Parse("15:30");
+            TimeSpan ts5 = TimeSpan.Parse("16:50");
+
+            TimeSpan[] ts = { ts1, ts2, ts3, ts4, ts5 };
+
+            int[] durations = { 60, 30, 10, 10, 40 };
+
+            TimeSpan ts6 = TimeSpan.Parse("8:00");
+            TimeSpan ts7 = TimeSpan.Parse("18:00");
+
+            var ch = new SF2022User_NN_Lib.Calculations();
+            string[] s = ch.AvailablePeriods(ts, durations, ts6, ts7, 30);
+
+            foreach (string s2 in s)
+            {
+                Console.WriteLine(s2);
+            }
+        }
+    }
+}
---
+++ SF2022User{NN}Lib/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User_NN_Lib
+{
+    public class Calculations
+    {
+        public string[] AvailablePeriods(TimeSpan[] startTimes,int[] durations,TimeSpan beginWorkingTime,TimeSpan endWorkingTime,int consultationTime)
+        {
+            int n = 0;
+            int z = 0;
+            string[] result = new string[14];
+            while (beginWorkingTime<=endWorkingTime)
+            {
+
+                if(beginWorkingTime==endWorkingTime)
+                {
+                    break;
+                }
+
+
+
+                //Console.WriteLine(beginWorkingTime);
+
+                //Console.WriteLine(startTimes[n]);
+                if ((beginWorkingTime >= startTimes[n].Subtract(TimeSpan.Parse("00:20")) && beginWorkingTime <= startTimes[n].Add(TimeSpan.Parse("00:20")))&& n !=5)
+                {
+                    beginWorkingTime = startTimes[n];
+
+                    if (durations[n] < 60)
+                    {
+                        Console.WriteLine(beginWorkingTime);
+                        beginWorkingTime = beginWorkingTime.Add(TimeSpan.Parse(String.Format("00:{0}", durations[n])));
+                    }
+                    else
+                    {
+                        Console.WriteLine(beginWorkingTime);
+                        beginWorkingTime = beginWorkingTime.Add(TimeSpan.Parse(String.Format("1:00", consultationTime)));
+
+                    }
+                    n++;
+                    if(n==5)
+                    {
+                        n = 0;
+                    }
+                    continue;
+                }
+                else
+                {
+                    beginWorkingTime = beginWorkingTime.Add(TimeSpan.Parse(String.Format("00:{0}", consultationTime)));
+                }
+
+
+                result[z] = String.Format("{1}-{0}", beginWorkingTime.ToString(), beginWorkingTime.Subtract(TimeSpan.Parse(String.Format("00:{0}", consultationTime))).ToString());
+                z++;
+
+            }
+
+
+            return result;
+        }
+    }
+}
